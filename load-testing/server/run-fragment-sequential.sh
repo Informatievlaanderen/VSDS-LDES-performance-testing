ddf0ed3c5b7ad7108fdc51948959009a25869b54
Ranko Orlic <ranko@aquila-solutions.com>
2024-11-14 11:47:34 +0100

    chore: add additional fragmentation tests

--- load-testing/server/run-fragment-sequential.sh
+++ load-testing/server/run-fragment-sequential.sh
@@ -2,3 +2,5 @@
 curl -X POST "$TEST_SERVER_BASE/test?category=$CATEGORY" -H "x-api-key: $X_API_KEY" -H "Content-Type: application/xml" --data-binary @./fragment.01V.jmx
 curl -X POST "$TEST_SERVER_BASE/test?category=$CATEGORY" -H "x-api-key: $X_API_KEY" -H "Content-Type: application/xml" --data-binary @./fragment.02V.jmx
 curl -X POST "$TEST_SERVER_BASE/test?category=$CATEGORY" -H "x-api-key: $X_API_KEY" -H "Content-Type: application/xml" --data-binary @./fragment.04V.jmx
+curl -X POST "$TEST_SERVER_BASE/test?category=$CATEGORY" -H "x-api-key: $X_API_KEY" -H "Content-Type: application/xml" --data-binary @./fragment.by-location.jmx
+curl -X POST "$TEST_SERVER_BASE/test?category=$CATEGORY" -H "x-api-key: $X_API_KEY" -H "Content-Type: application/xml" --data-binary @./fragment.by-time.jmx
